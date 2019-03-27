c05e77e7dd5de2f235ece4bde42da38141e8bfb1
chan0park <chan0park1@gmail.com>
2019-03-27 16:46:24 +0900

    initial commit

---
+++ src_ptr/config.py
@@ -0,0 +1,37 @@
+train_file_path = "../nn4nlp-summarization/data/try_out/pubmed/train.txt"
+val_file_path = "../nn4nlp-summarization/data/try_out/pubmed/val.txt"
+test_file_path = "../nn4nlp-summarization/data/try_out/pubmed/test.txt"
+vocab_file_path = "../nn4nlp-summarization/data/pubmed/vocab"
+log_root = "logs"
+
+# Hyperparameters
+hidden_dim= 256
+emb_dim= 128
+batch_size= 10
+max_enc_steps=2000
+max_dec_steps=210
+beam_size=4
+min_dec_steps=35
+vocab_size=50006
+
+max_section_len=500
+max_num_sections=4
+batch_shuffle_window=1
+seed=11
+
+lr=0.15
+adagrad_init_acc=0.1
+rand_unif_init_mag=0.02
+trunc_norm_init_std=1e-4
+max_grad_norm=2.0
+
+pointer_gen = True
+is_coverage = False
+cov_loss_wt = 1.0
+
+eps = 1e-12
+max_iterations = 500000
+
+use_gpu=True
+
+lr_coverage=0.15
---
+++ src_ptr/train.py
@@ -0,0 +1,152 @@
+from __future__ import unicode_literals, print_function, division
+
+# import ptvsd
+# ptvsd.enable_attach(address=('127.0.0.1', 99), redirect_output=True)
+# ptvsd.wait_for_attach()
+
+import os
+import time
+import argparse
+
+import torch
+from model import Model
+from torch.nn.utils import clip_grad_norm_
+from torch.optim import Adagrad
+from pre_process import DataLoader, batchify, Vocab
+import config
+from utils import get_input_from_batch, get_output_from_batch
+
+use_cuda = config.use_gpu and torch.cuda.is_available()
+
+class Train(object):
+    def __init__(self):
+        self.data = DataLoader(config)
+        self.vocab = self.data.vocab
+        train_dir = os.path.join(config.log_root, 'train_%d' % (int(time.time())))
+        if not os.path.exists(train_dir):
+            os.mkdir(train_dir)
+
+        self.model_dir = os.path.join(train_dir, 'model')
+        if not os.path.exists(self.model_dir):
+            os.mkdir(self.model_dir)
+    
+    def get_train_batches(self):
+        return batchify(self.data.get_training_examples(), config.batch_size, self.vocab)
+
+    def get_val_batches(self):
+        return batchify(self.data.get_val_examples(), config.batch_size, self.vocab)
+
+    def save_model(self, running_avg_loss, iter):
+        state = {
+            'iter': iter,
+            'encoder_state_dict': self.model.encoder.state_dict(),
+            'decoder_state_dict': self.model.decoder.state_dict(),
+            'reduce_state_dict': self.model.reduce_state.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'current_loss': running_avg_loss
+        }
+        model_save_path = os.path.join(self.model_dir, 'model_%d_%d' % (iter, int(time.time())))
+        torch.save(state, model_save_path)
+
+    def setup_train(self, model_file_path=None):
+        self.model = Model(model_file_path)
+
+        params = list(self.model.encoder.parameters()) + list(self.model.decoder.parameters()) + \
+                 list(self.model.reduce_state.parameters())
+        initial_lr = config.lr_coverage if config.is_coverage else config.lr
+        self.optimizer = Adagrad(params, lr=initial_lr, initial_accumulator_value=config.adagrad_init_acc)
+
+        start_iter, start_loss = 0, 0
+
+        if model_file_path is not None:
+            state = torch.load(model_file_path, map_location= lambda storage, location: storage)
+            start_iter = state['iter']
+            start_loss = state['current_loss']
+
+            if not config.is_coverage:
+                self.optimizer.load_state_dict(state['optimizer'])
+                if use_cuda:
+                    for state in self.optimizer.state.values():
+                        for k, v in state.items():
+                            if torch.is_tensor(v):
+                                state[k] = v.cuda()
+
+        return start_iter, start_loss
+
+    def train_one_batch(self, batch):
+        enc_batch, enc_padding_mask, enc_lens, enc_batch_extend_vocab, extra_zeros, c_t_1, coverage = \
+            get_input_from_batch(batch, use_cuda)
+        dec_batch, dec_padding_mask, max_dec_len, dec_lens_var, target_batch = \
+            get_output_from_batch(batch, use_cuda)
+
+        self.optimizer.zero_grad()
+
+        encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens)
+        s_t_1 = self.model.reduce_state(encoder_hidden)
+
+        step_losses = []
+        preds = []
+        for di in range(min(max_dec_len, config.max_dec_steps)):
+            y_t_1 = dec_batch[:, di]  # Teacher forcing
+            final_dist, s_t_1,  c_t_1, attn_dist, p_gen, next_coverage = self.model.decoder(y_t_1, s_t_1,
+                                                        encoder_outputs, encoder_feature, enc_padding_mask, c_t_1,
+                                                        extra_zeros, enc_batch_extend_vocab,
+                                                                           coverage, di)
+            target = target_batch[:, di]
+            preds.append(final_dist[0].argmax().item())
+            gold_probs = torch.gather(final_dist, 1, target.unsqueeze(1)).squeeze()
+            step_loss = -torch.log(gold_probs + config.eps)
+            if config.is_coverage:
+                step_coverage_loss = torch.sum(torch.min(attn_dist, coverage), 1)
+                step_loss = step_loss + config.cov_loss_wt * step_coverage_loss
+                coverage = next_coverage
+                
+            step_mask = dec_padding_mask[:, di]
+            step_loss = step_loss * step_mask
+            step_losses.append(step_loss)
+
+        sum_losses = torch.sum(torch.stack(step_losses, 1), 1)
+        batch_avg_loss = sum_losses/dec_lens_var
+        loss = torch.mean(batch_avg_loss)
+
+        loss.backward()
+
+        self.norm = clip_grad_norm_(self.model.encoder.parameters(), config.max_grad_norm)
+        clip_grad_norm_(self.model.decoder.parameters(), config.max_grad_norm)
+        clip_grad_norm_(self.model.reduce_state.parameters(), config.max_grad_norm)
+
+        self.optimizer.step()
+
+        return loss.item(), preds
+
+    def trainIters(self, n_iters, model_file_path=None):
+        iter, running_avg_loss = self.setup_train(model_file_path)
+        start = time.time()
+        while iter < n_iters:
+            # batch = self.batcher.next_batch()
+            batches = self.get_train_batches()
+            for batch in batches:
+                loss, pred = self.train_one_batch(batch)
+                iter += 1
+
+                print_interval = 50
+                if iter % print_interval == 0:
+                    print('steps %d, %d batch: %.2fs , loss: %f' % (iter, print_interval,
+                                                                            time.time() - start, loss))
+                    start = time.time()
+                    print("output: "+" ".join([self.vocab.get(x) for x in pred]))
+                    print(f"target: {' '.join(batch.abstracts[0].words)}")
+                if iter % 10000 == 0:
+                    self.save_model(running_avg_loss, iter)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Train script")
+    parser.add_argument("-m",
+                        dest="model_file_path", 
+                        required=False,
+                        default=None,
+                        help="Model file for retraining (default: None).")
+    args = parser.parse_args()
+    
+    train_processor = Train()
+    train_processor.trainIters(config.max_iterations, args.model_file_path)
