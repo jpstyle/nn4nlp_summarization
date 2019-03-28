1894284ca390ff78ba82a4260b2523484591003d
chan0park <chan0park1@gmail.com>
2019-03-28 20:17:30 +0900

    added makechunk file

---
+++ src_final/make_chunk.py
@@ -0,0 +1,26 @@
+import os
+import struct
+chunks_dir = os.path.join("pubmed_clean", "chunked_quarter")
+CHUNK_SIZE = 1000 # num examples per chunk, for the chunked data
+	
+def chunk_file(set_name):
+    in_file = 'pubmed_clean/%s.bin' % set_name
+    reader = open(in_file, "rb")
+    chunk = 0
+    finished = False
+    while not finished:
+        chunk_fname = os.path.join(chunks_dir, '%s_%03d.bin' % (set_name, chunk)) # new chunk
+        with open(chunk_fname, 'wb') as writer:
+            for _ in range(CHUNK_SIZE):
+                len_bytes = reader.read(8)
+                if not len_bytes:
+                    finished = True
+                    break
+                str_len = struct.unpack('q', len_bytes)[0]
+                example_str = struct.unpack('%ds' % str_len, reader.read(str_len))[0]
+                writer.write(struct.pack('q', str_len))
+                writer.write(struct.pack('%ds' % str_len, example_str))
+            chunk += 1
+
+# os.mkdir(chunks_dir)
+chunk_file("train_clean_fourth")
